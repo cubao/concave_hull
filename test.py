a46b353a2246b0ab39a09fa5ff77abcaef42a49c
TANG ZHIXIONG <dvora4tzx@gmail.com>
2022-11-04 18:52:27 +0800

    fix

--- test.py
+++ test.py
@@ -2,16 +2,7 @@ import matplotlib.pyplot as plt
 import numpy as np
 from scipy.spatial import ConvexHull
 
-from concave_hull import LineSegment, concave_hull_indexes
-
-seg = LineSegment([0, 0, 0], [10, 0, 0])
-assert 4.0 == seg.distance([5.0, 4.0, 0.0])
-assert 5.0 == seg.distance([-4.0, 3.0, 0.0])
-assert 5.0 == seg.distance([14.0, 3.0, 0.0])
-seg = LineSegment([0, 0, 0], [0, 0, 0])
-assert 5.0 == seg.distance([3.0, 4.0, 0.0])
-assert 5.0 == seg.distance([-4.0, 3.0, 0.0])
-assert 13.0 == seg.distance([5.0, 12.0, 0.0])
+from concave_hull import concave_hull_indexes
 
 points = []
 c = np.array([250, 250])
