849aeb8fe2bb81ee9bf7f3d4b81b4245db7de8ca
TANG ZhiXiong <district10@users.noreply.github.com>
2022-11-04 19:31:55 +0800

    release 0.0.2 (#5)
    Co-authored-by: TANG ZHIXIONG <dvora4tzx@gmail.com>

--- setup.py
+++ setup.py
@@ -122,11 +122,11 @@ class CMakeBuild(build_ext):
 # logic and declaration, and simpler if you include description/version in a file.
 setup(
     name="concave_hull",
-    version="0.0.1",
+    version="0.0.2",
     author="tzx",
     author_email="dvorak4tzx@gmail.com",
     url="https://github.com/cubao/concave_hull",
-    description="C++/pybind11/NumPy implementation of the Ramer-Douglas-Peucker algorithm (Ramer 1972; Douglas and Peucker 1973) for 2D and 3D data.",
+    description="A very fast 2D concave hull algorithm",
     long_description=open("README.md", encoding="utf-8").read(),
     long_description_content_type="text/markdown",
     ext_modules=[CMakeExtension("concave_hull")],
