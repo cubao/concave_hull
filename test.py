fbd6ffd8bfba5206afd7866f98b32e48b50c5f9f
TANG ZHIXIONG <dvora4tzx@gmail.com>
2022-11-04 13:37:53 +0800

    add test

--- test.py
+++ test.py
@@ -1,11 +1,8 @@
-import time
-
+import matplotlib.pyplot as plt
 import numpy as np
-from rdp import rdp as rdp_python
+from scipy.spatial import ConvexHull
 
-from concave_hull import LineSegment
-from concave_hull import rdp as rdp_pybind
-from concave_hull import rdp_mask as rdp_mask
+from concave_hull import LineSegment, concave_hull_indexes
 
 seg = LineSegment([0, 0, 0], [10, 0, 0])
 assert 4.0 == seg.distance([5.0, 4.0, 0.0])
@@ -16,75 +13,31 @@ assert 5.0 == seg.distance([3.0, 4.0, 0.0])
 assert 5.0 == seg.distance([-4.0, 3.0, 0.0])
 assert 13.0 == seg.distance([5.0, 12.0, 0.0])
 
-for fn in [rdp_python, rdp_pybind]:
-    print("#" * 80)
-    print(fn)
+points = []
+c = np.array([250, 250])
+for x in np.arange(100, 400, 5 * np.pi):
+    for y in np.arange(100, 400, 5 * np.pi):
+        if x > c[0] and y > c[1]:
+            continue
+        r = np.linalg.norm(c - [x, y])
+        if r > 150:
+            continue
+        points.append([x, y])
+points = np.array(points)
+convex_hull = ConvexHull(points)
 
-    print()
-    coords = np.array([[1, 1], [2, 2], [3, 3], [4, 4]], dtype=np.float64)
-    print(coords)
-    print(fn(coords))
+# https://docs.scipy.org/doc/scipy/reference/generated/scipy.spatial.ConvexHull.html
 
-    print()
-    coords = np.array([[1, 1], [1, 1.1], [2, 2]], dtype=np.float64)
-    print(coords)
-    print(fn(coords, epsilon=0.5))
+plt.plot(points[:, 0], points[:, 1], "o")
+for simplex in convex_hull.simplices:
+    plt.plot(points[simplex, 0], points[simplex, 1], "k-")
 
-enus = np.load("data/enus.npy")
-assert enus.shape == (2000, 3)
-print("enus")
-print(enus)
+idxes = concave_hull_indexes(
+    points[:, :2],
+    convex_hull_indexes=convex_hull.vertices.astype(np.int32),
+)
 
-durations = []
-
-
-def my_timer(func):
-    def wrapper(*arg, **kw):
-        t1 = time.time()
-        res = func(*arg, **kw)
-        t2 = time.time()
-        delta = max(t2 - t1, 1e-18)
-        print(f"{delta} seconds")
-        durations.append(delta)
-        return res
-
-    return wrapper
-
-
-@my_timer
-def python_rdp(xyzs: np.ndarray, *, epsilon: float = 0.0, return_mask: bool = False):
-    return rdp_python(xyzs, epsilon=epsilon, return_mask=return_mask)
-
-
-@my_timer
-def cpp_rdp(
-    xyzs: np.ndarray, *, epsilon: float = 0.0, return_mask: bool = False
-) -> np.ndarray:
-    return (
-        rdp_pybind(xyzs, epsilon=epsilon)
-        if not return_mask
-        else rdp_mask(xyzs, epsilon=epsilon)
-    )
-
-
-print("benchmarking... python/c++ version")
-for return_mask in (True, False):
-    for eps in (10.0, 5.0, 2.0, 1.0, 0.5, 0.1, 0.0):
-        for dim in (3, 2):
-            print("\n" * 4)
-            ret1 = python_rdp(enus[:, :dim], epsilon=eps, return_mask=return_mask)
-            ret2 = cpp_rdp(enus[:, :dim], epsilon=eps, return_mask=return_mask)
-            print(f"mask?: {return_mask}, epsilon: {eps}, dim: {dim}")
-            print(ret1.shape, ret2.shape)
-            print(f"speed up: {durations[-2] / durations[-1]:.2f}")
-            if return_mask:
-                if len(ret1) != len(ret2):
-                    raise Exception(f"mismatch! {len(ret1)} != {len(ret2)}")
-                else:
-                    ret1 = ret1.astype(np.int32)
-                    ret2 = ret2.astype(np.int32)
-                    delta = np.abs(ret1 - ret2).sum()
-                    if delta != 0:
-                        raise Exception(f"mismatch! {ret1 - ret2}")
-            delta = np.abs(np.array(ret1) - np.array(ret2)).sum()
-            assert delta == 0
+for f, t in zip(idxes[:-1], idxes[1:]):
+    seg = points[[f, t]]
+    plt.plot(seg[:, 0], seg[:, 1], "k-")
+plt.show()
